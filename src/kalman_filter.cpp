a866598ae1141aae3a21fdcfb3ece239094b6886
saipmedi-nbcuni <saiprateek.medi@nbcuni.com>
2017-09-10 19:08:53 +0100

    fixed project code

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -22,6 +22,10 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+    x_ = F_ * x_;
+    MatrixXd Ft = F.transpose();
+    P_ = F_ * P_ * Ft + Q_;
+
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -29,11 +33,62 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
-}
+
+    // calculating error for KF function
+    VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+    // calculating Kalman matrix
+    MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+
+	}
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+    /* transform measurements from to do measurement update from polar coordinates
+     Radar measurements need to undergo h(x) function
+
+    px is x_(0) || py is x_(1)
+    vx is x_(2) || vy is x_(3)
+
+
+    rho = sqrt(x^2+y^2)
+    phi = arctan(y/x)
+    drdt=(x*v1_y*vy)/rho ^^
+    */
+    double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+    double phi = atan(x_(1) / x_(0));
+    double drdt = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
+    VectorXd h = VectorXd(3);
+    h << rho, theta, drdt;
+
+    VectorXd y = z - h;
+
+    // calculating Kalman matrix
+    MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+
+
+
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,37 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+  	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+    // TODO: YOUR CODE HERE
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	// ... your code here
+    if (estimations.size() != ground_truth.size() ) {
+
+        cout << " The input does not match for estimations versus ground truth values" << endl;
+        return rmse;
+    }
+
+	//accumulate squared residuals
+	for(int i=0; i < estimations.size(); ++i){
+        // ... your code here
+		rmse +=  ( (estimations[i]-ground_truth[i])^2 ) estimations/size ;
+
+
+	}
+	//calculate the mean
+	// ... your code here
+    rmse = rmse/ground_truth.size();
+	//calculate the squared root
+	// ... your code here
+    rmse = sqrt(rmse);
+	//return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +53,45 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+	MatrixXd Hj(3,4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//TODO: YOUR CODE HERE
+    //Hj(0,0)
+    float px2 = px*px;
+    float py2 = py*py;
+
+    float denom = (px2+py2);
+    float sqdenom = sqrt(denom);
+    float wierdenom = denom * sqdenom ;
+    Hj(0,2) = 0;
+    Hj(0,3) = 0;
+    Hj(1,2) = 0;
+    Hj(1,3) = 0;
+
+    //flag for divides by 0
+
+    bool dividebyzero = false;
+
+    if (sqdenom < 0.0001 ) {
+        dividebyzero = true;
+        cout << "CalculateJacobian () - Error - Division by Zero is " <<dividebyzero << endl;
+        return Hj;
+    } else {
+        Hj(0,0) = px/sqdenom ;
+        Hj(0,1) = py/sqdenom ;
+        Hj(1,0) = -py/denom ;
+        Hj(1,1) = px/denom ;
+        Hj(2,0) = py*(vx*py - vy*px) /wierdenom ;
+        Hj(2,1) = px*(px*vy - py*vx) / wierdenom ;
+        Hj(2,2) = px/sqdenom;
+        Hj(2,3) = py/sqdenom;
+    }
+    return Hj;
+
+
 }
